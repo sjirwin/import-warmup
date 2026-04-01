31845b46e88be7d20ce78de1d4b2ae8c3d1dcfbd
Scott Irwin <sjirwin@users.noreply.github.com>
2026-04-01 21:20:06 +0000

    move calculate_portfolio_value and calculate_asset_value to separate module

---
+++ portfolio/metrics.py
@@ -0,0 +1,8 @@
+def calculate_portfolio_value(portfolio: dict) -> float:
+    total = 0
+    for asset in portfolio["assets"]:
+        total += calculate_asset_value(asset)
+    return total
+
+def calculate_asset_value(asset: dict) -> float:
+    return asset["price"] * asset["quantity"]
--- portfolio/report.py
+++ portfolio/report.py
@@ -1,15 +1,8 @@
+from .metrics import calculate_portfolio_value
+
 def print_report(portfolio: list[dict]) -> None:
     total = calculate_portfolio_value(portfolio)
     print(f"PORTFOLIO SUMMARY\nPortfolio Name: {portfolio["name"]}")
     print("--------------------")
     print(f"Number of assets: {len(portfolio["assets"])}")
     print(f"Total value: ${total:,.2f}")
-
-def calculate_portfolio_value(portfolio: dict) -> float:
-    total = 0
-    for asset in portfolio["assets"]:
-        total += calculate_asset_value(asset)
-    return total
-
-def calculate_asset_value(asset: dict) -> float:
-    return asset["price"] * asset["quantity"]
